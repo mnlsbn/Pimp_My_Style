380ce3cb5a6b31b75ee92042776609a94f727bad
manuel sabine <manuel.sabine@gmail.com>
2017-06-28 12:04:07 +0200

    Adds Brand model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628095821) do
+ActiveRecord::Schema.define(version: 20170628100242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
