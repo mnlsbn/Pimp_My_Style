0f4de871b1244059857f14147c9be2fbb81aa162
manuel sabine <manuel.sabine@gmail.com>
2017-06-28 19:07:58 +0200

    start article_index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 #-----Destroy_all----#
 puts "Delete all seeds..."
 Article.destroy_all
+Brand.destroy_all
 
 #-----Brand----#
 puts "Creating brands..."
@@ -20,7 +21,7 @@ armelle = Article.create!(
   name: "Armelle",
   description: "Caraco tissé double épaisseur",
   price: 17,
-  photo_url: "https://res.cloudinary.com/dqzbzynrl/image/upload/v1498659612/caraco_tissé_lkkipf.jpg",
+  photo_url: "http://images.asos-media.com/products/boohoo-top-volante-a-encolure-bardot/8188648-1-white?$XXL$&wid=513&fit=constrain",
   brand_id: 1,
   category_id: 3
 )
@@ -29,7 +30,7 @@ brigitte = Article.create!(
   name: "Brigitte",
   description: "Débardeur volanté au dos à imprimé petites fleurs noires",
   price: 28,
-  photo_url: "https://res.cloudinary.com/dqzbzynrl/image/upload/v1498659613/debardeur_volanté_uxvvgn.jpg",
+  photo_url: "http://images.asos-media.com/products/boohoo-top-volante-a-encolure-bardot/8188648-1-white?$XXL$&wid=513&fit=constrain",
   brand_id: 2,
   category_id: 1
 )
@@ -38,7 +39,7 @@ caroline = Article.create!(
   name: "Caroline",
   description: "Débardeur avec joli effet côtelé",
   price: 11,
-  photo_url: "https://res.cloudinary.com/dqzbzynrl/image/upload/v1498659614/debardeur_cotelé_qvcfo8.jpg",
+  photo_url: "http://images.asos-media.com/products/boohoo-top-volante-a-encolure-bardot/8188648-1-white?$XXL$&wid=513&fit=constrain",
   brand_id: 1,
   category_id: 1
 )
@@ -47,7 +48,7 @@ delphine = Article.create!(
   name: "Delphine",
   description: "Body manches courtes à épaules dénudées",
   price: 15,
-  photo_url: "https://res.cloudinary.com/dqzbzynrl/image/upload/v1498659612/body_manches_courtes_c2qz1z.jpg",
+  photo_url: "http://images.asos-media.com/products/boohoo-top-volante-a-encolure-bardot/8188648-1-white?$XXL$&wid=513&fit=constrain",
   brand_id: 2,
   category_id: 2
 )
@@ -56,7 +57,7 @@ elodie = Article.create!(
   name: "Elodie",
   description: "Body cache-cœur à épaules dénudées",
   price: 14,
-  photo_url: "https://res.cloudinary.com/dqzbzynrl/image/upload/v1498659613/body_cache_coeur_nni4ok.jpg",
+  photo_url: "http://images.asos-media.com/products/boohoo-top-volante-a-encolure-bardot/8188648-1-white?$XXL$&wid=513&fit=constrain",
   brand_id: 1,
   category_id: 2
 )
@@ -65,7 +66,7 @@ fiona = Article.create!(
   name: "Fiona",
   description: "Top volanté à encolure Bardot",
   price: 15.5,
-  photo_url: "https://res.cloudinary.com/dqzbzynrl/image/upload/v1498659613/top_bardot_bobtd1.jpg",
+  photo_url: "http://images.asos-media.com/products/boohoo-top-volante-a-encolure-bardot/8188648-1-white?$XXL$&wid=513&fit=constrain",
   brand_id: 2,
   category_id: 4
 )
